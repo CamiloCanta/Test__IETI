2f9ec332077d0525d7a60222dccd0f7b83ef722a
camilo cantillo <camiloct35@gmail.com>
2023-09-22 18:19:13 -0500

    test

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "b6d36ac5c4b361a373f8dca513059866";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public WeatherReport getWeatherReport(double latitude, double longitude) {
